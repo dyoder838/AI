03375087ca9bafc64d2f520faa5ff5defc244727
Dyoder838 <dyoder838@gmail.com>
2023-07-23 15:59:53 -0700

    Configured base intry point to openAI

---
+++ script.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const { OpenAI } = require('langchain/llms/openai');
+
+const model = new OpenAI({ 
+    openAIApiKey: process.env.OPENAI_API_KEY, 
+    temperature: 0,
+    model: 'gpt-3.5-turbo'
+  });
+  
