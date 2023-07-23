dd8cd26d1947131c0226c9be052f963020f6a9aa
Dyoder838 <dyoder838@gmail.com>
2023-07-23 16:22:23 -0700

    added an inquirer prompt for CLI interface with openAI

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 require('dotenv').config();
-
+const inquirer = require('inquirer');
 const { OpenAI } = require('langchain/llms/openai');
 
-const model = new OpenAI({ 
-    openAIApiKey: process.env.OPENAI_API_KEY, 
-    temperature: 0,
-    model: 'gpt-3.5-turbo'
-  });
-  
-  const promptFunc = async () => {
+// connection to openAI API
+const model = new OpenAI({
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  temperature: 0,
+  model: 'gpt-3.5-turbo'
+});
 
-    try {
-      const res = await model.call("How do you capitalize all characters of a string in JavaScript?");
-      console.log(res);
-    }
-    catch (err) {
-      console.log(err);
-    }
-  };
-  
-  promptFunc();
+
+// API input function
+const promptFunc = async (input) => {
+
+  try {
+    const res = await model.call(input);
+    console.log(res);
+  }
+  catch (err) {
+    console.log(err);
+  }
+};
+
+
+// inquirer prompt
+const init = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Ask a question',
+    },
+
+  ]).then((inquirerResponse) => {
+    promptFunc(inquirerResponse.name)
+  });
+};
+
+// initialize the script
+init();
