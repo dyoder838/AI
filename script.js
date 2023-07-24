3f9e0ffc394e5e04d7957a1aebec39a9caea36b5
Dyoder838 <dyoder838@gmail.com>
2023-07-23 17:13:07 -0700

    added templating via langchan to narow the scope of user inquires

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const inquirer = require('inquirer');
 const { OpenAI } = require('langchain/llms/openai');
+const { PromptTemplate } = require("langchain/prompts");
 
 // connection to openAI API
 const model = new OpenAI({
@@ -16,6 +17,16 @@ const promptFunc = async (input) => {
   try {
     const res = await model.call(input);
     console.log(res);
+
+    // incorporating langchain templates. this sets the specific rules for the AIs response.
+    const prompt = new PromptTemplate({
+      template: "You are a javascript expert and will answer the user’s coding questions thoroughly as possible.\n{question}",
+      inputVariables: ["question"],
+    });
+
+    const promptInput = await prompt.format({
+      question: input
+    });
   }
   catch (err) {
     console.log(err);
